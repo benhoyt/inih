3cc0891238841da44105b30b70d7485a18d1eb2e
benhoyt <benhoyt@f5d6dc10-6d35-11de-b131-07d8e4d3762e>
2009-07-10 10:11:38 +0000

    Committing simple config file example as per project home page.

---
+++ trunk/ini_example.c
@@ -0,0 +1,39 @@
+/* Example: parse a simple configuration file */
+
+#include <stdio.h>
+#include <string.h>
+#include "ini.h"
+
+typedef struct
+{
+    int version;
+    const char* name;
+    const char* email;
+} configuration;
+
+static int handler(void* user, const char* section, const char* name, const char* value)
+{
+    configuration* pconfig = (configuration*)user;
+
+    #define MATCH(s, n) stricmp(section, s) == 0 && stricmp(name, n) == 0
+    if (MATCH("protocol", "version")) {
+        pconfig->version = atoi(value);
+    } else if (MATCH("user", "name")) {
+        pconfig->name = strdup(value);
+    } else if (MATCH("user", "email")) {
+        pconfig->email = strdup(value);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    configuration config;
+
+    if (ini_parse("test.ini", handler, &config) < 0) {
+        printf("Can't load 'test.ini'\n");
+        return 1;
+    }
+    printf("Config loaded from 'test.ini': version=%d, name=%s, email=%s\n",
+        config.version, config.name, config.email);
+    return 0;
+}
