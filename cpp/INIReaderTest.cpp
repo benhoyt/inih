37732b84a8bab802c8caf52901734a1f6db28b6d
benhoyt <benhoyt@f5d6dc10-6d35-11de-b131-07d8e4d3762e>
2009-08-20 23:32:33 +0000

    Added X-Macros example. Simplified INIReaderTest.cpp.

--- cpp/INIReaderTest.cpp
+++ cpp/INIReaderTest.cpp
@@ -1,25 +1,19 @@
-// INIReader.h example that shows simple usage of the INIReader class
+// Example that shows simple usage of the INIReader class
 
 #include <iostream>
 #include "INIReader.h"
-
-using std::cout;
 
 int main()
 {
     INIReader reader("../examples/test.ini");
 
     if (reader.ParseError() < 0) {
-        cout << "Couldn't read INI file!\n";
+        std::cout << "Can't load 'test.ini'\n";
         return 1;
     }
-
-    cout << "Valid config:";
-    cout << " user_name='" << reader.Get("user", "name", "UNKNOWN") << "'";
-    cout << " user_email='" << reader.Get("user", "email", "UNKNOWN") << "'";
-    cout << " protocol_version="
-         << reader.GetInteger("protocol", "version", -1);
-    cout << "\n";
-
+    std::cout << "Config loaded from 'test.ini': version="
+              << reader.GetInteger("protocol", "version", -1) << ", name="
+              << reader.Get("user", "name", "UNKNOWN") << ", email="
+              << reader.Get("user", "email", "UNKNOWN") << "\n";
     return 0;
 }
