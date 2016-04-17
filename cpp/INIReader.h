ccd77e50db8baf4034bae2c8e8d66b626acfdebb
Artem Shelest <artem.e.shelest@gmail.com>
2016-04-17 08:33:48 +0300

    Make INIReader const correct.

--- cpp/INIReader.h
+++ cpp/INIReader.h
@@ -22,25 +22,25 @@ public:
 
     // Return the result of ini_parse(), i.e., 0 on success, line number of
     // first error on parse error, or -1 on file open error.
-    int ParseError();
+    int ParseError() const;
 
     // Get a string value from INI file, returning default_value if not found.
     std::string Get(std::string section, std::string name,
-                    std::string default_value);
+                    std::string default_value) const;
 
     // Get an integer (long) value from INI file, returning default_value if
     // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
-    long GetInteger(std::string section, std::string name, long default_value);
+    long GetInteger(std::string section, std::string name, long default_value) const;
 
     // Get a real (floating point double) value from INI file, returning
     // default_value if not found or not a valid floating point value
     // according to strtod().
-    double GetReal(std::string section, std::string name, double default_value);
+    double GetReal(std::string section, std::string name, double default_value) const;
 
     // Get a boolean value from INI file, returning default_value if not found or if
     // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
     // and valid false values are "false", "no", "off", "0" (not case sensitive).
-    bool GetBoolean(std::string section, std::string name, bool default_value);
+    bool GetBoolean(std::string section, std::string name, bool default_value) const;
 
 private:
     int _error;
