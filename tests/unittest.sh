12758aae01de3bf2106071a17c388f48c9603e45
Ben Hoyt <bhoyt@jetsetter.com>
2016-10-11 09:33:13 -0400

    Delete unittest binaries after use

--- tests/unittest.sh
+++ tests/unittest.sh
@@ -2,9 +2,16 @@
 
 gcc ../ini.c unittest.c -o unittest_multi
 ./unittest_multi > baseline_multi.txt
+rm -f unittest_multi
+
 gcc ../ini.c -DINI_ALLOW_MULTILINE=0 unittest.c -o unittest_single
 ./unittest_single > baseline_single.txt
+rm -f unittest_single
+
 gcc ../ini.c -DINI_ALLOW_INLINE_COMMENTS=0 unittest.c -o unittest_disallow_inline_comments
 ./unittest_disallow_inline_comments > baseline_disallow_inline_comments.txt
+rm -f unittest_disallow_inline_comments
+
 gcc ../ini.c -DINI_STOP_ON_FIRST_ERROR=1 unittest.c -o unittest_stop_on_first_error
 ./unittest_stop_on_first_error > baseline_stop_on_first_error.txt
+rm -f unittest_stop_on_first_error
