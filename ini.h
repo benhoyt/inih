92c54fd33623ec592e19fe6e96c90ce777860e74
WilliamFeely <WilliamFeely@users.noreply.github.com>
2017-07-09 01:32:23 -0400

    Describe ini_parse_unicode

--- ini.h
+++ ini.h
@@ -49,6 +49,23 @@ typedef char* (*ini_reader)(char* str, int num, void* stream);
    error (only when INI_USE_STACK is zero).
 */
 int ini_parse(const char* filename, ini_handler handler, void* user);
+
+/* Parse given INI-style file. May have [section]s, name=value pairs
+   (whitespace stripped), and comments starting with ';' (semicolon). Section
+   is "" if name=value pair parsed before any section heading. name:value
+   pairs are also supported as a concession to Python's configparser.
+
+   For each name=value pair parsed, call handler function with given user
+   pointer as well as section, name, and value (data only valid for duration
+   of handler call). Handler should return nonzero on success, zero on error.
+
+   Returns 0 on success, line number of first error on parse error (doesn't
+   stop on first error), -1 on file open error, or -2 on memory allocation
+   error (only when INI_USE_STACK is zero).
+   
+   This version of the function loads an INI-style file from a Unicode path;
+   however the callback still uses ANSI style strings.
+*/
 int ini_parse_unicode(const wchar_t* filename, ini_handler handler, void* user);
 
 /* Same as ini_parse(), but takes a FILE* instead of filename. This doesn't
