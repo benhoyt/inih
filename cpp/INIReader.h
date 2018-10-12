2023872dfffb38b6a98f2c45a0eb25652aaea91f
NeatNit <NeatNit@gmail.com>
2018-10-12 16:58:40 +0300

    Add INIReader::GetString to get nicer default_value behavior (#72)
    * add INIReader::GetString
    
    Same as INIReader::Get, but returns default_value if the value is an empty string, which provides functionality similar to GetInteger and the others
    
    * clarify GetString description comment

--- cpp/INIReader.h
+++ cpp/INIReader.h
@@ -27,6 +27,11 @@ public:
     // Get a string value from INI file, returning default_value if not found.
     std::string Get(const std::string& section, const std::string& name,
                     const std::string& default_value) const;
+    
+    // Get a string value from INI file, returning default_value if not found,
+    // empty, or contains only whitespace.
+    std::string GetString(const std::string& section, const std::string& name,
+                    const std::string& default_value) const;
 
     // Get an integer (long) value from INI file, returning default_value if
     // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
