37eaa28b33ff5980e8ab97971dc9876cc710fbb3
Norbert Lange <norbert.lange@andritz.com>
2020-06-17 10:21:09 +0200

    Disable variable prev_name and accesses if uneeded
    If INI_ALLOW_MULTILINE is not defined, then this variable
    if not read anywhere.

--- ini.c
+++ ini.c
@@ -97,7 +97,9 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
     size_t offset;
 #endif
     char section[MAX_SECTION] = "";
+#if INI_ALLOW_MULTILINE
     char prev_name[MAX_NAME] = "";
+#endif
 
     char* start;
     char* end;
@@ -170,7 +172,9 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
             if (*end == ']') {
                 *end = '\0';
                 strncpy0(section, start + 1, sizeof(section));
+#if INI_ALLOW_MULTILINE
                 *prev_name = '\0';
+#endif
 #if INI_CALL_HANDLER_ON_NEW_SECTION
                 if (!HANDLER(user, section, NULL, NULL) && !error)
                     error = lineno;
@@ -197,7 +201,9 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
                 rstrip(value);
 
                 /* Valid name[=:]value pair found, call handler */
+#if INI_ALLOW_MULTILINE
                 strncpy0(prev_name, name, sizeof(prev_name));
+#endif
                 if (!HANDLER(user, section, name, value) && !error)
                     error = lineno;
             }
