bd798c53775a4aefaf8ac42091bface3e876d190
Ben Hoyt <benhoyt@gmail.com>
2022-07-12 21:20:05 +1200

    Make inline comments work on subsequent lines of multiline values (#140)
    This is a bug fix that makes inih's behaviour here work as per Python's
    ConfigParser, which is what inih behaviour is (loosely) based on.
    
    Thanks @silly2020 for the report. See
    https://github.com/benhoyt/inih/issues/120#issuecomment-1181341960

--- ini.c
+++ ini.c
@@ -171,6 +171,12 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
         }
 #if INI_ALLOW_MULTILINE
         else if (*prev_name && *start && start > line) {
+#if INI_ALLOW_INLINE_COMMENTS
+            end = find_chars_or_comment(start, NULL);
+            if (*end)
+                *end = '\0';
+            rstrip(start);
+#endif
             /* Non-blank line with leading whitespace, treat as continuation
                of previous name's value (as per Python configparser). */
             if (!HANDLER(user, section, prev_name, start) && !error)
