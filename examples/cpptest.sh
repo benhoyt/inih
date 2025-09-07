841620c454853911c637eeaace171b25e75b9781
Mike Squire <mike.squire@gmail.com>
2025-09-08 00:43:52 +0100

    Added a simple test for the INIReader::ParseErrorMessage() method.

--- examples/cpptest.sh
+++ examples/cpptest.sh
@@ -3,3 +3,7 @@
 g++ -Wall INIReaderExample.cpp ../cpp/INIReader.cpp ../ini.c -o INIReaderExample
 ./INIReaderExample > cpptest.txt
 rm INIReaderExample
+
+g++ -Wall INIReaderExampleErrors.cpp ../cpp/INIReader.cpp ../ini.c -o INIReaderExampleErrors
+./INIReaderExampleErrors > cpptesterrors.txt
+rm INIReaderExampleErrors
